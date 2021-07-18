ff7d8cd11bc9a19fde1b03b63037ec94b5b0aac2
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2021-07-18 23:48:52 +0200

    Add metrics dashboard

--- start.js
+++ start.js
@@ -1,5 +1,9 @@
 const app = require('./app');
+var dash = require('appmetrics-dash');
 
 const server = app.listen(process.env.PORT, () => {
   console.log(`Express is running on port ${server.address().port}`);
 });
+
+// Start monitoring dashboard
+dash.monitor({server: server});
