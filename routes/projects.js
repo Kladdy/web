eda9d4cdedf3beb13e47db5d7ef5b8aa17ffcf34
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2021-11-04 22:40:15 +0100

    Test revert

--- routes/projects.js
+++ routes/projects.js
@@ -1,14 +1,11 @@
 const express = require('express');
 // const { check } = require('express-validator');
 const router = express.Router();
-const { track } = require('../segment_analytics/analytics')
 
 const BachelorController = require('../controllers/projects_controller');
 
 get_bachelor_project_url = '/bachelor_project'
 router.get(get_bachelor_project_url, (req, res) => {
-    track(req, 'get_bachelor_project', get_bachelor_project_url)
-    
     BachelorController.get(req, res)
 });
 
--- segment_analytics/analytics.js
+++ segment_analytics/analytics.js
@@ -1,26 +1,7 @@
-const { get_ip_details } = require('../routes/get_ip_details')
-var useragent = require('useragent');
-// useragent(true); // Always keep the RegExp library up to date
+
 var Analytics = require('analytics-node');
 
 var analytics = new Analytics(process.env.SEGMENTKEY || process.env.SEGMENTKEYDEV);
 
-const track = (req, event, url) => {
-    var agent = useragent.parse(req.headers['user-agent']);
-
-    analytics.track({
-        anonymousId: req.sessionID,
-        event: event,
-        properties: {
-            url: url,
-            env: process.env.WEBENV,
-            os: agent?.toAgent(),
-            browser: agent?.os.toString(),
-            device: agent?.device.toString(),
-            ...get_ip_details(req),
-        },
-    })
-}
 
 module.exports.analytics = analytics
-module.exports.track = track
