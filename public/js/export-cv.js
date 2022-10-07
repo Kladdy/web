c57a2aa9b3bc1a746143b8b433c5cb4ad97a460b
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2022-10-08 00:39:53 +0200

    Add script to remove footer and header

---
+++ public/js/export-cv.js
@@ -0,0 +1,5 @@
+export_cv = function() {
+  // Hide the nav component
+  document.getElementById('navbar_to_hide').style.display = 'none';
+  document.getElementById('cv_footer_to_hide').style.display = 'none';
+}
