1b833c0f80a28a8ecd1c54103324658cd5ac53ed
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2022-10-07 20:20:54 +0200

    Remove tack

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,11 @@
 const express = require('express');
-// const { check } = require('express-validator');
 const router = express.Router();
-const { track } = require('../segment_analytics/analytics')
 
 const IndexController = require('../controllers/index_controller');
 
 get_index_url = '/'
 router.get(get_index_url, (req, res) => {
-    track(req, 'get_home', get_index_url)
-
     IndexController.get(req, res)
 });
-
-// router.post('/',
-// [
-//     check('name')
-//     .isLength({ min: 1 })
-//     .withMessage('Please enter a name'),
-//     check('email')
-//     .isLength({ min: 1 })
-//     .withMessage('Please enter an email'),
-// ], (req, res) => {
-//     IndexController.post(req, res)
-// });
 
 module.exports = router;
--- routes/projects.js
+++ routes/projects.js
@@ -1,27 +1,11 @@
 const express = require('express');
-// const { check } = require('express-validator');
 const router = express.Router();
-const { track } = require('../segment_analytics/analytics')
 
 const BachelorController = require('../controllers/projects_controller');
 
 get_bachelor_project_url = '/bachelor_project'
 router.get(get_bachelor_project_url, (req, res) => {
-    track(req, 'get_bachelor_project', get_bachelor_project_url)
-
     BachelorController.get(req, res)
 });
-
-// router.post('/',
-// [
-//     check('name')
-//     .isLength({ min: 1 })
-//     .withMessage('Please enter a name'),
-//     check('email')
-//     .isLength({ min: 1 })
-//     .withMessage('Please enter an email'),
-// ], (req, res) => {
-//     IndexController.post(req, res)
-// });
 
 module.exports = router;
